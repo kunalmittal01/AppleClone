ad5447ed15b2692940ded684bc72b12140e299a1
kunalmittal01 <kunalmittalmrt@gmail.com>
2024-09-30 00:16:23 +0530

    navbar modified.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const sliderWrapper = document.querySelector('.slider-wrapper');
 const navDots = document.querySelectorAll('.slider-nav li');
-
+const store = document.querySelectorAll('.store-icon');
+const storelist = document.getElementsByClassName('store-list')[0];
 // Add event listeners to each dot
 navDots.forEach((dot, index) => {
     dot.addEventListener('click', () => {
@@ -18,3 +19,17 @@ navDots.forEach((dot, index) => {
         dot.classList.add('active');
     });
 });
+
+store.forEach(ele => {
+    ele.addEventListener('mouseover' ,()=>{
+        storelist.style.height = '18rem';
+        storelist.style.padding= '2rem';
+        document.querySelector('nav').style.backgroundColor = '#161617';
+    })
+    
+    ele.addEventListener('mouseout' ,()=>{
+        storelist.style.height = '0rem'
+        storelist.style.padding= '0rem';
+        document.querySelector('nav').style.backgroundColor = '#454545';
+    })
+})
